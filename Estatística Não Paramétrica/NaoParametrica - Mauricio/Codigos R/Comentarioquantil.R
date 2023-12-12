04ec9fb4ae3770d65b2965897ae7ec9a1d20aa66
Romulo Freitas <romulofrts0@gmail.com>
2023-12-12 17:32:33 -0300

    Create Comentarioquantil.R

---
+++ Estatística Não Paramétrica/NaoParametrica - Mauricio/Codigos R/Comentarioquantil.R
@@ -0,0 +1,22 @@
+##Exemplo 3.5 do Bussab&Morettin
+
+
+X=c(15,5,3,8,10,2,7,11,12
+n=length(X);n
+Y=sort(X);Y
+
+
+###D1;D2;Q1;Q2;Q_3)
+
+
+###Resposta do Bussab usando a alisada!!!!!
+
+
+BM=c(2.4,3.6,4.5,8,11.25)
+
+qr5=quantile(X,c(0.1,0.2,0.25,0.5,0.75),type=5);qr5 
+
+cbind(BM,qr5)
+
+
+###O tipo 5 gera os quantis a partir da alisada do BM. 
